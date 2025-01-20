757c21c842c1813efe587a1776b4ea4e6ac7e3f1
Max <max.maksim.max@mail.ru>
2025-01-20 18:48:43 +0300

    организация классов кораблей: Добавлены классы: Entity - класс-родитель для классов Enemy и Player В классы добавлены основные параметры

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ import pygame
 import csv
 
 
-def load_image(name, color_key=None):
-    fullname = os.path.join("data", name)
+def load_image(name, directory, color_key=None):
+    fullname = os.path.join("data/" + directory, name)
     if not os.path.isfile(fullname):
         print(f"file with image '{fullname}' not found")
         sys.exit()
@@ -22,9 +22,33 @@ def load_image(name, color_key=None):
     return image
 
 
-class Entity:
-    def __init__(self):
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, group, x, y):
+        super().__init__(group)
+        self.health = 999
+        self.image = pygame.transform.scale(load_image("none.png", "entity", color_key=-1), (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        if self.health <= 0:
+            self.kill()
+
+
+class Enemy(Entity):
+    pass
+
+
+class Player(Entity):
+    def __init__(self, group, x, y):
+        super().__init__(group, x, y)
+        self.health = 3
+        self.image = load_image("plane.png", "entity", color_key=-1)
+
+    def update(self):
         pass
+
 
 class GameScene:
     def __init__(self, width, height):
@@ -33,15 +57,15 @@ class GameScene:
         self.max_score = int(open("max_score.csv", encoding="utf-8").read().split("\n")[1])
 
         # GameLevel
-        self.images = os.listdir("data")
+        self.images = os.listdir("data/background")
         self.width = width
         self.height = height
         self.size = width, height
         self.number_of_image = 1
         self.y_pos1 = 0
         self.y_pos2 = -height
-        self.image1 = pygame.transform.scale(load_image(self.images[0]), self.size)
-        self.image2 = pygame.transform.scale(load_image(self.images[1]), self.size)
+        self.image1 = pygame.transform.scale(load_image(self.images[0], "background"), self.size)
+        self.image2 = pygame.transform.scale(load_image(self.images[1], "background"), self.size)
         self.alternation = 1
         self.background_speed = 0.5
 
@@ -58,7 +82,7 @@ class GameScene:
                     self.number_of_image = 0
                 else:
                     self.number_of_image += 1
-                self.image1 = pygame.transform.scale(load_image(self.images[self.number_of_image]), self.size)
+                self.image1 = pygame.transform.scale(load_image(self.images[self.number_of_image], "background"), self.size)
                 self.alternation = 2
         else:
             if self.y_pos2 >= self.height:
@@ -67,7 +91,7 @@ class GameScene:
                     self.number_of_image = 0
                 else:
                     self.number_of_image += 1
-                self.image2 = pygame.transform.scale(load_image(self.images[self.number_of_image]), self.size)
+                self.image2 = pygame.transform.scale(load_image(self.images[self.number_of_image], "background"), self.size)
                 self.alternation = 1
 
     def get_max_score(self):
@@ -82,7 +106,8 @@ if __name__ == "__main__":
     width, height = size = 1000, 600
     screen = pygame.display.set_mode(size)
     GameScene = GameScene(width, height)
-    print(GameScene.get_max_score())
+    all_sprites = pygame.sprite.Group()
+    player = Player(all_sprites, 100, 300)
 
     running = True
     while running:
@@ -90,6 +115,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
         GameScene.update_background(screen)
+        all_sprites.draw(screen)
         pygame.display.flip()
 
     if GameScene.get_max_score() > int(open("max_score.csv", encoding="utf-8").read().split("\n")[1]):
